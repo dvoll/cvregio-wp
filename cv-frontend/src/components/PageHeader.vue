efdfec850662f1f6afd960a1bb95628aef6f681a
Dario Voll <dvoll@uandi.com>
2019-05-17 17:14:28 +0200

    Add link to header title and logo

--- cv-frontend/src/components/PageHeader.vue
+++ cv-frontend/src/components/PageHeader.vue
@@ -1,34 +1,20 @@
 <template>
     <div class="page-header" :class="{ 'page-header--small': small }">
         <div class="page-header__body">
-            <img class="page-header__logo" :src="logoUrl" />
-            <div class="page-header__title-wrapper">
-                <span class="page-header__title">{{ title }}</span>
-                <span class="page-header__subtitle">{{ subtitle }}</span>
-            </div>
-            <!-- <page-nav class="page-header__nav"> -->
+            <a href="/" class="page-header__link">
+                <img class="page-header__logo" :src="logoUrl" />
+                <div class="page-header__title-wrapper">
+                    <span class="page-header__title">{{ title }}</span>
+                    <span class="page-header__subtitle">{{ subtitle }}</span>
+                </div>
+            </a>
             <slot name="default" class="page-header__nav" />
-            <!-- <template slot="default"> -->
-            <!-- <slot name="nav">-header nav slot-</slot>
-                <slot>-de-</slot>
-                Default2 -->
-            <!-- <div v-html="$slots.default[0].data.domProps.innerHTML"></div> -->
-            <!-- </template> -->
-            <!-- </page-nav> -->
         </div>
     </div>
 </template>
 
 <script>
-// @ts-check
-// import CvNav from './CvNav.vue';
-
-// const initialHeaderHeight = 95;
-// const smallHeaderHeight = 50;
 const CvHeaderElement = {
-    components: {
-        // CvNav,
-    },
     props: {
         title: {
             type: String,
@@ -45,48 +31,6 @@ const CvHeaderElement = {
             default: '../assets/logo-sq.png',
         },
     },
-    methods: {
-        // addScrollEventListener() {
-        //     let last_known_scroll_position = 0;
-        //     let ticking = false;
-        //     window.addEventListener('scroll', e => {
-        //         last_known_scroll_position = window.scrollY;
-        //         if (!ticking) {
-        //             window.requestAnimationFrame(() => {
-        //                 // const progress = this.getRelativeHeaderTransformationProgress(
-        //                 //     last_known_scroll_position
-        //                 // );
-        //                 this.changeHeader(last_known_scroll_position);
-        //                 ticking = false;
-        //             });
-        //             ticking = true;
-        //         }
-        //     });
-        // },
-        // changeHeader(position) {
-        //     if (!this.smallHeader && position > smallHeaderHeight) {
-        //         this.smallHeader = true;
-        //     }
-        //     if (this.smallHeader && position < smallHeaderHeight) {
-        //         this.smallHeader = false;
-        //     }
-        // },
-        // getRelativeHeaderTransformationProgress(scrollPosition) {
-        //     if (scrollPosition > initialHeaderHeight) return 1;
-        //     return scrollPosition / initialHeaderHeight;
-        // },
-        // updateTitlePosition() {},
-        // updateHeaderHeight(position) {
-        //     if (position > smallHeaderHeight) {
-        //         return;
-        //     }
-        //     this.headerHeight = initialHeaderHeight - position;
-        // },
-    },
-    mounted() {
-        // console.log('slots', this.$slots);
-        // this.addScrollEventListener();
-    },
 };
 export default CvHeaderElement;
 </script>
@@ -97,31 +41,13 @@ $smallHeaderHeight: 50;
 $transitionSpeedGrow: 0.3s;
 $transitionSpeedShrink: 0.4s ease-out;
 
-// page-header {
-//     display: block;
-//     height: #{$initialHeaderHeight} + 'px';
-// }
-
 .page-header {
-    // position: fixed;
-    // z-index: 100;
-    // top: 0;
-    // left: 0;
-    // width: 100%;
-    // // padding: 0 50px;
-    // height: #{$initialHeaderHeight} + 'px';
-    // transition: transform 0.3s;
-    // border-bottom: 1px solid #fff;
-    // width: 100%;
-    // background: #fff;
-    // // overflow: hidden;
-
-    // &--small {
-    //     // height: 50px;
-    //     border-color: #ccc;
-    //     transform: translateY(#{$smallHeaderHeight - $initialHeaderHeight} + 'px');
-    //     transition: transform #{$transitionSpeedShrink};
-    // }
+    &__link {
+        text-decoration: none;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
 
     &__body {
         height: 100%;
@@ -135,33 +61,21 @@ $transitionSpeedShrink: 0.4s ease-out;
         padding: 0 50px;
 
         .page-header--small & {
-            // transform: translate(-30px, #{($initialHeaderHeight - $smallHeaderHeight) / 2} + 'px');
             transform: translateY(#{($initialHeaderHeight - $smallHeaderHeight) / 2} + 'px');
             transition: transform #{$transitionSpeedShrink};
         }
     }
-    // padding: 12.5px 0;
 
     &__logo {
-        // height: calc(100% - 25px);
-        // position: absolute;
         height: #{$initialHeaderHeight - 25} + 'px';
-        // height: 100%;
-        // width: 80px;
         width: auto;
-        // align-self: stretch;
         margin: 12.5px 5px;
-        // margin: 0 5px;
-        // background: rgb(192, 25, 25);
-        // background-image: url('../../public/placeholder/logo-sq.png');
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center center;
-        // transition: height 0.5s;
         transition: transform 0.3s;
 
         .page-header--small & {
-            // height: #{$smallHeaderHeight - 25} + 'px';
             transform: scale(0.5);
             transition: transform #{$transitionSpeedShrink};
         }
@@ -181,6 +95,7 @@ $transitionSpeedShrink: 0.4s ease-out;
         font-size: 20px;
         transition: transform 0.15s; // cubic-bezier(0.47, 0, 0.74, 0.71);
         line-height: 1.4rem;
+        color: #000;
 
         .page-header--small & {
             transform: translate(-40px, 9px) scale(0.9);
