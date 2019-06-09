91e5435f9b6d9494395221a00921f4d1c4b7ade8
Dario Voll <dvoll@uandi.com>
2019-06-09 19:58:02 +0200

    Add base components to app

--- cv-frontend/src/main.js
+++ cv-frontend/src/main.js
@@ -5,6 +5,8 @@ import vueCustomElement from 'vue-custom-element';
 import CvStageElement from './components/CvStageElement.vue';
 import CvHeaderElement from './components/CvHeaderElement.vue';
 
+import './base-components';
+
 console.log('Frontend loaded.');
 
 Vue.config.productionTip = false;
