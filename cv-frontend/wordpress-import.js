7245f840d49a3d7b39bace42459bb9f2276078bd
Dario Voll <dvoll@uandi.com>
2019-06-09 13:15:11 +0200

    Add new import for plugin and theme

--- cv-frontend/wordpress-import.js
+++ cv-frontend/wordpress-import.js
@@ -2,9 +2,6 @@
 
 const fsp = require('fs').promises;
 
-const pluginDir = 'cv-blocks/libs/';
-const themeDir = 'cv-regio-theme';
-
 let fileContent = `<?php
 /* generated */
 
@@ -13,111 +10,148 @@ if ( ! defined( 'ABSPATH' ) ) {
 } \n
 `;
 
-/**
- * @param {string} name
- * @param {string} type TOOD: add source map option sourceMap = false
- * @param {string} dist
- */
-async function getFileAndCopy(name, type, dist) {
-    const fileExp = new RegExp(name + '\\.\\w*\\.' + type + '$');
-    const dir = './dist/' + type;
+const srcDir = './dist/';
+
+const mapping = [
+    {
+        name: 'plugin',
+        targetPath: './../plugins/cv-blocks/',
+        distFolder: 'cv-frontend',
+        files: [
+            { type: 'js', name: 'chunk-vendors' },
+            { type: 'js', name: 'chunk-common' },
+            { type: 'js', name: 'app' },
+            { type: 'css', name: 'chunk-vendors' },
+            { type: 'css', name: 'chunk-common' },
+            { type: 'css', name: 'app' },
+        ],
+    },
+    {
+        name: 'theme',
+        targetPath: './../themes/cv-regio-theme/',
+        distFolder: 'cv-frontend',
+        files: [
+            { type: 'js', name: 'chunk-vendors' },
+            { type: 'js', name: 'chunk-common' },
+            { type: 'js', name: 'app' },
+            { type: 'css', name: 'chunk-vendors' },
+            { type: 'css', name: 'chunk-common' },
+            { type: 'css', name: 'app' },
+        ],
+    },
+];
+
+main(srcDir);
+
+function getWpDeclaration(file, dist) {
+    if (file.type === 'js') {
+        return `
+wp_enqueue_script('cv-frontend-${file.chunkName}', plugins_url( '${dist +
+            file.fileName}', dirname( __FILE__ )), array(), '1.0', true );
+`;
+    }
+    return `
+wp_register_style(
+    'cv-frontend-${file.chunkName}',
+    plugins_url( '${dist + file.fileName}', dirname( __FILE__ ) ),
+    array( 'wp-editor' )
+);
+wp_enqueue_style('cv-frontend-${file.chunkName}');
+`;
+}
+
+async function copyFile(src, target) {
     try {
-        let files = await fsp.readdir(dir);
-        const file = files.filter(file => {
-            return file.match(fileExp);
-        })[0];
-        await fsp.copyFile('./dist/' + type + '/' + file, dist + file);
-        return file;
+        await fsp.copyFile(src, target);
+        console.log(`Copied file: ${src} -> ${target} `);
     } catch (err) {
-        console.error('Error on file read and copy: ', err);
+        console.error('Error on copying file: ', err);
         return;
     }
 }
 
-/**
- * @param {string} content
- */
-async function writeFile(content) {
-    const appDist = 'frontend/';
-    const pluginDir = './../plugins/cv-blocks/';
-
+async function clearFolder(dir) {
     try {
-        await fsp.mkdir(pluginDir + appDist);
+        await fsp.mkdir(dir);
         console.log('Direction cleaned');
     } catch {
-        console.log('Direction frontend already exsits');
+        console.log(`Direction ${dir} already exsits`);
     }
     try {
-        let files = await fsp.readdir(pluginDir + appDist);
+        let files = await fsp.readdir(dir);
         files.forEach(async file => {
             try {
-                await fsp.unlink(pluginDir + appDist + file);
+                await fsp.unlink(dir + file);
             } catch (err) {
                 console.error('Error on deleting file ' + file + ': ', err);
             }
         });
-        console.log('Direction cleaned');
+        console.log(`Direction ${dir} cleaned`);
     } catch (err) {
-        console.error('Error clearing dir: ', err);
-    }
-
-    const vendorCss = await getFileAndCopy('chunk-vendors', 'css', pluginDir + appDist);
-    content += `
-wp_register_style(
-  'cv-frontend-vendor-css',
-  plugins_url( '${appDist + vendorCss}', dirname( __FILE__ ) ),
-  array( 'wp-editor' )
-);
-wp_enqueue_style('cv-frontend-vendor-css');
-  `;
-
-    const commonCss = await getFileAndCopy('chunk-common', 'css', pluginDir + appDist);
-    content += `
-wp_register_style(
-  'cv-frontend-common-css',
-  plugins_url( '${appDist + commonCss}', dirname( __FILE__ ) ),
-  array( 'wp-editor' )
-);
-wp_enqueue_style('cv-frontend-common-css');
-  `;
-
-    const appCss = await getFileAndCopy('app', 'css', pluginDir + appDist);
-    content += `
-wp_enqueue_style(
-  'cv-frontend-app-css',
-  plugins_url( '${appDist + appCss}', dirname( __FILE__ ) ),
-  array( 'wp-editor' )
-);
-  `;
-
-    const vendorJs = await getFileAndCopy('chunk-vendors', 'js', pluginDir + appDist);
-    content += `
-wp_register_script('cv-frontend-vendor-js', plugins_url( '${appDist +
-        vendorJs}', dirname( __FILE__ )), array(), '1.0', true );
-wp_enqueue_script('cv-frontend-vendor-js');
-`;
-
-    const commonJs = await getFileAndCopy('chunk-common', 'js', pluginDir + appDist);
-    content += `
-wp_register_script('cv-frontend-common-js', plugins_url( '${appDist +
-        commonJs}', dirname( __FILE__ )), array(), '1.0', true );
-wp_enqueue_script('cv-frontend-common-js');
-`;
-    const appJs = await getFileAndCopy('app', 'js', pluginDir + appDist);
-    content += `
-wp_enqueue_script('cv-frontend-app-js', plugins_url( '${appDist +
-        appJs}', dirname( __FILE__ )), array(), '1.0', true );
-  `;
-
-    try {
-        await fsp.writeFile(pluginDir + appDist + 'frontend-include.php', content);
-        console.log('Write app file successfull.');
-    } catch (err) {
-        console.error('Error on writing file: ', err);
+        console.error(`Error clearing dir ${dir} : `, err);
     }
 }
 
-writeFile(fileContent);
-//#region app
+async function main(srcDirection) {
+    const files = await getFiles([srcDirection + 'css', srcDirection + 'js']);
+    for (const mapItem of mapping) {
+        console.log();
+        console.log(`Start processing for ${mapItem.name}`);
+        await clearFolder(mapItem.targetPath + mapItem.distFolder + '/');
+        let wpdeclaration = '';
+        for (const mapFile of mapItem.files) {
+            const file = files.find(
+                file => file.chunkName === mapFile.name && file.type === mapFile.type
+            );
+            if (file) {
+                copyFile(
+                    srcDir + file.type + '/' + file.fileName,
+                    mapItem.targetPath + mapItem.distFolder + '/' + file.fileName
+                );
+                wpdeclaration += getWpDeclaration(file, mapItem.distFolder + '/');
+            }
+        }
+        const wpFileContent = fileContent + wpdeclaration;
+        try {
+            await fsp.writeFile(
+                mapItem.targetPath + mapItem.distFolder + '/' + 'frontend-include.php',
+                wpFileContent
+            );
+            console.log(`Written succesfully to ${mapItem.name}.`);
+        } catch (err) {
+            console.error(`Error on writing file to : ${mapItem.name}`, err);
+        }
+    }
+}
 
-//#endregion app
+async function getFiles(dirs) {
+    if (!dirs) {
+        console.warn('No directories to read.');
+        return [];
+    }
+    let files = [];
+    for (let i = 0; i < dirs.length; i++) {
+        const dir = dirs[i];
+        files = files.concat(await readDir(dir));
+    }
+    files = files.map(file => {
+        const content = file.split('.');
+        return {
+            chunkName: content[0],
+            type: content.includes(3) ? content[2] + content[3] : content[2],
+            fileName: file,
+        };
+    });
+    return files;
+}
+
+async function readDir(dir) {
+    let files = [];
+    try {
+        files = await fsp.readdir(dir);
+    } catch (err) {
+        console.error('Error reding directory: ', err);
+        return [];
+    }
+    return files;
+}
