c35850149831a787c83a58d5589485f1424ea81a
Dario Voll <dvoll@uandi.com>
2019-06-10 18:37:35 +0200

    Change cv-blocks plugin short info

--- plugins/cv-blocks/plugin.php
+++ plugins/cv-blocks/plugin.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Plugin Name: cv-blocks — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: cv-blocks — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: CV Regio Blocks
+ * Plugin URI: http://cvjm-stift-quernheim.de/
+ * Description: Inhalts-Elemente für die CV_regio Seite.
+ * Author: dvoll
+ * Author URI: http://cvjm-stift-quernheim.de/
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package CGB
  */
 
 // Exit if accessed directly.
