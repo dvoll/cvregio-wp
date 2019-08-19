4dcbf6d71d487cb2957d9b81a3325a5dde14b91e
Dario Voll <dvoll@uandi.com>
2019-08-19 19:31:03 +0200

    Change cvgroups template

--- themes/cv-regio-theme/single-cvgroups.php
+++ themes/cv-regio-theme/single-cvgroups.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * NOTUSEDTemplate Post Type: cvgroups
  * 
@@ -8,28 +9,23 @@
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main site-main--homepage">
+<section id="primary" class="content-area">
+	<main id="main" class="site-main site-main--homepage">
 
-            <h2>CV groups</h2>
-			<?php
+		<?php
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		/* Start the Loop */
+		while (have_posts()) :
+			the_post();
 
-                get_template_part( 'template-parts/content/content', 'page' );
+			get_template_part('template-parts/content/content', 'page');
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
 
-			endwhile; // End of the loop.
-			?>
+		endwhile; // End of the loop.
+		?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main><!-- #main -->
+</section><!-- #primary -->
 
 <?php
 get_footer();
