a20be8c76d70d2e758d7585909e910426bc318a3
Dario Voll <dariovoll@gmail.com>
2019-08-30 00:16:50 +0200

    Change sidebar behavior

--- plugins/cv-blocks/cv-frontend/frontend-include.php
+++ plugins/cv-blocks/cv-frontend/frontend-include.php
@@ -8,9 +8,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 wp_enqueue_script('cv-frontend-chunk-vendors', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-vendors.4f15e53b.js', array(), '1.0', true );
 
-wp_enqueue_script('cv-frontend-chunk-common', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-common.59138603.js', array(), '1.0', true );
+wp_enqueue_script('cv-frontend-chunk-common', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-common.f45b7455.js', array(), '1.0', true );
 
-wp_enqueue_script('cv-frontend-plugin', content_url() . '/plugins/cv-blocks/cv-frontend/plugin.4b0ae44b.js', array(), '1.0', true );
+wp_enqueue_script('cv-frontend-plugin', content_url() . '/plugins/cv-blocks/cv-frontend/plugin.fb6703af.js', array(), '1.0', true );
 
 wp_register_style(
     'cv-frontend-plugin',
