d1385fa14fff458b157ddd47c4948ee18c4200d5
Dario Voll <dvoll@uandi.com>
2019-10-18 21:03:31 +0200

    Add build

--- plugins/cv-blocks/cv-frontend/frontend-include.php
+++ plugins/cv-blocks/cv-frontend/frontend-include.php
@@ -6,11 +6,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 } 
 
 
-wp_enqueue_script('cv-frontend-chunk-vendors', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-vendors.4f15e53b.js', array(), '1.0', true );
+wp_enqueue_script('cv-frontend-chunk-vendors', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-vendors.c857d096.js', array(), '1.0', true );
 
-wp_enqueue_script('cv-frontend-chunk-common', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-common.6c731e91.js', array(), '1.0', true );
+wp_enqueue_script('cv-frontend-chunk-common', content_url() . '/plugins/cv-blocks/cv-frontend/chunk-common.89517d81.js', array(), '1.0', true );
 
-wp_enqueue_script('cv-frontend-plugin', content_url() . '/plugins/cv-blocks/cv-frontend/plugin.fb6703af.js', array(), '1.0', true );
+wp_enqueue_script('cv-frontend-plugin', content_url() . '/plugins/cv-blocks/cv-frontend/plugin.3bc0f5ee.js', array(), '1.0', true );
 
 wp_register_style(
     'cv-frontend-plugin',
