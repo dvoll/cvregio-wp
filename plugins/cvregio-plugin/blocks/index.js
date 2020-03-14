77e39248f667a6ecbe023f7ad4366c813c7cdf62
Dario Voll <dvoll@uandi.com>
2020-03-14 16:37:27 +0100

    Remove js files from plugin

--- plugins/cvregio-plugin/blocks/index.js
+++
@@ -1,6 +0,0 @@
-// import './blocks/cv-stage-item';
-// import './blocks/cv-stage';
-// import './blocks/cv-groups';
-// import './blocks/cv-group-detail';
-// import './blocks/cv-sidebar';
-// import './blocks/cv-news';
