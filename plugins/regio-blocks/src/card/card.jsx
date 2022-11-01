f224271802bc96f2d2dcfb6997d0d0b9017c0829
Dario Voll <dariovoll@gmail.com>
2022-11-01 18:28:44 +0100

    Fix card link edit class

--- plugins/regio-blocks/src/card/card.jsx
+++ plugins/regio-blocks/src/card/card.jsx
@@ -14,17 +14,17 @@ import ArrowIcon from './arrow-icon';
 const Card = ({ title, content, linkUrl = undefined, image = undefined, onClick }) => {
     return (
         <div
-            className="wp-block-ev-region22-blocks-card-link ev-region22-blocks-card-link has-background-background-color has-background"
+            className="wp-block-ev-region22-blocks-card-link ev-region22-card has-background-background-color has-background"
             style={{ borderRadius: '0.25rem', paddingTop: 0, paddingBottom: '1rem', pointerEvents: 'none' }}
             onClick={onClick}
         >
             {image && image.url && (
-                <div className="wp-block-ev-region22-blocks-card-link__image-container">
-                    <img className="wp-block-ev-region22-blocks-card-link__image" src={image.url} alt={image.alt} />
+                <div className="ev-region22-card__image-container">
+                    <img className="ev-region22-card__image" src={image.url} alt={image.alt} />
                 </div>
             )}
-            <div className="wp-block-ev-region22-blocks-card-link__content">
-                <div className="wp-block-ev-region22-blocks-card-link__inner-blocks-wrapper">
+            <div className="ev-region22-card__content">
+                <div className="ev-region22-card__inner-blocks-wrapper">
                     <h3
                         className="has-text-color"
                         style={{
@@ -42,22 +42,12 @@ const Card = ({ title, content, linkUrl = undefined, image = undefined, onClick 
                     ></div>
                 </div>
                 {linkUrl && (
-                    <a
-                        className="wp-block-ev-region22-blocks-card-link__link"
-                        aria-label="Artikel aufrufen"
-                        href={linkUrl}
-                    >
+                    <a className="ev-region22-card__link" aria-label="Artikel aufrufen" href={linkUrl}>
                         <ArrowIcon />
                     </a>
                 )}
             </div>
-            {linkUrl && (
-                <a
-                    className="wp-block-ev-region22-blocks-card-link__linkclickarea"
-                    aria-hidden="true"
-                    href={linkUrl}
-                ></a>
-            )}
+            {linkUrl && <a className="ev-region22-card__linkclickarea" aria-hidden="true" href={linkUrl}></a>}
         </div>
     );
 };
